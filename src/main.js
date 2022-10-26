c01ce85ba5a5942d045fc69a121a28943ac066aa
Hobby <g99949@hotmail.com>
2022-10-26 11:11:38 +0800

    revise: cron 改用 element ui 套件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import router from './router'
-import cronLight from '@vue-js-cron/light'
-import '@vue-js-cron/light/dist/light.css'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import CronElementPlus from '@vue-js-cron/element-plus'
+import '@vue-js-cron/element-plus/dist/element-plus.css'
 
 createApp(App)
     .use(router)
-    .use(cronLight)
+    .use(CronElementPlus)
+    .use(ElementPlus)
     .mount('#app')
