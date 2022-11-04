e546c185fdc88253d554e7a3018bf3aa63f33815
q90016200 <q90016200@gmail.com>
2022-11-04 17:01:58 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Scheduling from "../views/Scheduling.vue";
